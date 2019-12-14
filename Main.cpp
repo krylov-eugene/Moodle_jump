8de12479c08c07194a9077e1282b27e88ef99d95
kudaxech <55214057+kudaxech@users.noreply.github.com>
2019-12-14 09:32:18 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,6 @@
 #include "Person.h"
 
 
-/*
-const int window_heighth = 650, window_width = 400,
-		platform_heighth = 10, platform_width = 30,
-		platforms_rate = 100;
-*/
-
-
 bool is_mouse_position_in_district_of_button(const vector2I& mouse ,const vector2I& button , const int& button_height,const int& button_width){
 
 	if(mouse.x >= button.x && mouse.x <= button.x + button_width &&
@@ -21,54 +14,32 @@ bool is_mouse_position_in_district_of_button(const vector2I& mouse ,const vector
 
 	return false;	
 
-}	
+}
+void update_all_objects(List_of_platforms& platforms,Person& person,float& height,float dt){
+	platforms.update(height,dt);
+	person.update(platforms,dt,height,window_height);
+}
+void draw_all_objects(sf::RenderWindow& window,List_of_platforms& platforms,Person& person,sf::Sprite& background){
 
-class Images : public DrawableObject {
-
-	public:
-
-		sf::Sprite person,screen,platform_0;
-		sf::Texture moodler,background,desk_0;
-
-		Images(){
-
+	window.clear(); 
 			
+	window.draw(background);
+	platforms.draw(window);
+	person.draw(window);
+	
+	window.display();
 
-			moodler.loadFromFile("images/moodler.png");
-			if(!background.loadFromFile("images/background.png")){
-				
-			}
-			desk_0.loadFromFile("images/platform_0.png");
+}
 
-			
-			platform_0.setTexture(desk_0);
-			person.setTexture(moodler);
-			screen.setTexture(background);
+bool is_losing_game(float y_position_of_moodler){
+	if(y_position_of_moodler >= window_height){
+		return true;
+	}
 
-		}
+	return false;
 
-		sf::Sprite get_person(){
+}
 
-			return person;
-		}
-
-		sf::Sprite& get_screen(){
-
-			return screen;
-		}
-
-		sf::Sprite& get_platform(){
-
-			return platform_0;
-		}
-		void Draw(sf::RenderWindow* window){
-
-			window->clear(sf::Color(0,200,0));
-			
-		}
-    private:
-
-};
 
 class Main_menu{
 
@@ -88,10 +59,14 @@ class Main_menu{
 		void draw_main_menu_picture(sf::RenderWindow& window , bool is_restart_game_button_pressed){
 			
 			if(is_restart_game_button_pressed){
+				window.clear();
 				window.draw(pressed_button);
+				window.display();
 			}
 			else{
+				window.clear();
 				window.draw(not_pressed_button);
+				window.display();
 			}
 
 		}
@@ -115,13 +90,11 @@ class Main_menu{
 				y_position_of_mouse = event_of_pressed_button.mouseButton.y;
 				
 			}
+
 			if(event_of_released_button.type == sf::Event::MouseButtonReleased){
 				is_mouse_button_released = true;
 			}
-
-					
-				
-
+		
 			if(is_mouse_position_in_district_of_button({x_position_of_mouse,y_position_of_mouse},{x_position_of_button,y_position_of_button},button_height,button_widht) 
 			    && is_mouse_button_pressed)
 			   {
@@ -134,30 +107,26 @@ class Main_menu{
 			return is_restart_game_button_pressed && is_mouse_button_released;
 
 		}
-
-
-
-
+		
 	private:
 
-		double x_position_of_button = 50,y_position_of_button = 500 ,
-				button_height = 100 ,button_widht = 200;
+		double x_position_of_button = 100,y_position_of_button = 550 ,
+				button_height = 50 ,button_widht = 200;
 
 		sf::Texture for_pressed_button,for_not_pressed_button;
 		sf::Sprite pressed_button , not_pressed_button;
 };
 
-void main_cycle(sf::RenderWindow& window, List_of_platforms& platforms,GraphicManager& manager,Person& person)
+void main_cycle(sf::RenderWindow& window, List_of_platforms& platforms,Person& person)
 {
 		float height = 0;
 		sf::Clock clock;
 
+
 		sf::Texture background_texture;
 		background_texture.loadFromFile("images/background.png");
 		sf::Sprite background;
-
 		background.setTexture(background_texture);
-
 
 
 		while (window.isOpen())
@@ -165,44 +134,23 @@ void main_cycle(sf::RenderWindow& window, List_of_platforms& platforms,GraphicMa
 			float dt = clock.getElapsedTime().asSeconds();
 			clock.restart();
 
-			sf::Event event_of_press_moving_button;
 			sf::Event event;
 			while (window.pollEvent(event))
 			{
 				if (event.type == sf::Event::Closed){
 					window.close();
-				}
-				if(event.type == sf::Event::KeyPressed){
-
-					event_of_press_moving_button = event;
-					break;
-            	}	   
+				}	   
 			}
 
-
-			// --logics--
-
-			// update objcts
-
-			platforms.update(height,dt);
-			person.update(event_of_press_moving_button,platforms,dt,height,window_height);
-
-			// --drawing--
-
-			window.clear(); // or background
-			// draw objects
-			window.draw(background);
-			person.draw(window);
-			platforms.draw(window);
-			
+			if(is_losing_game(person.get_y_position())){
+				break;
+			}
+			update_all_objects(platforms,person,height,dt);
 
 
-			window.display();
-			
+			draw_all_objects(window,platforms,person,background);
 		}
 }	
-
-
 
 int main(){
 
@@ -212,14 +160,6 @@ int main(){
 
 	Person person;
 	List_of_platforms platforms;
-	
-
-    GraphicManager manager;
-	manager.Register(&platforms);
-	manager.Register(&person);
-
-	
-	
 
 	
 	sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Moodle Jump");
@@ -228,16 +168,18 @@ int main(){
 	
 	
 	while (window.isOpen()){
-
+		
 		
 		sf::Event event_of_released_button;
 		sf::Event event_of_pressed_button;
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			if (event.type == sf::Event::Closed){  
 
+				window.close();
+				
+			}
 			if(event.type == sf::Event::MouseButtonPressed){
 
 				event_of_pressed_button = event;
@@ -246,22 +188,221 @@ int main(){
 			if(event.type == sf::Event::MouseButtonReleased){
 
 				event_of_released_button = event;
-			}
-			
-			
+
+			}	
 		}
 
-		window.clear(sf::Color(235,215,245)); // or background
 		
 		if(menu.is_restart_game_button_pressed_and_released(window,event_of_pressed_button,event_of_released_button)){
-			main_cycle(window,platforms,manager,person);
-			
+			main_cycle(window,platforms,person);
 		}
-
-		window.display();
-		
+	
+	
 	}
 
+	return 0;
+}#include <SFML/Graphics.hpp>
+#include "List_of_platforms.h"
+#include "vector2I.h"
+#include "Person.h"
+
+
+bool is_mouse_position_in_district_of_button(const vector2I& mouse ,const vector2I& button , const int& button_height,const int& button_width){
+
+	if(mouse.x >= button.x && mouse.x <= button.x + button_width &&
+		mouse.y >= button.y && mouse.y <= button.y + button_height )
+		{
+			return true;
+		}
+
+	return false;	
+
+}
+void update_all_objects(List_of_platforms& platforms,Person& person,float& height,float dt){
+	platforms.update(height,dt);
+	person.update(platforms,dt,height,window_height);
+}
+void draw_all_objects(sf::RenderWindow& window,List_of_platforms& platforms,Person& person,sf::Sprite& background){
+
+	window.clear(); 
+			
+	window.draw(background);
+	platforms.draw(window);
+	person.draw(window);
+	
+	window.display();
+
+}
+
+bool is_losing_game(float y_position_of_moodler){
+	if(y_position_of_moodler >= window_height){
+		return true;
+	}
+
+	return false;
+
+}
+
+
+class Main_menu{
+
+	public:
+
+
+		Main_menu(){
+
+			for_pressed_button.loadFromFile("images/menu_pressed.png");
+			for_not_pressed_button.loadFromFile("images/menu.png");
+
+			not_pressed_button.setTexture(for_not_pressed_button);
+			pressed_button.setTexture(for_pressed_button);
+
+		}
+
+		void draw_main_menu_picture(sf::RenderWindow& window , bool is_restart_game_button_pressed){
+			
+			if(is_restart_game_button_pressed){
+				window.clear();
+				window.draw(pressed_button);
+				window.display();
+			}
+			else{
+				window.clear();
+				window.draw(not_pressed_button);
+				window.display();
+			}
+
+		}
+
+		bool is_restart_game_button_pressed_and_released(sf::RenderWindow& window, sf::Event event_of_pressed_button ,sf::Event event_of_released_button ){
+
+			bool is_mouse_button_pressed = false;
+			bool is_mouse_button_released = false;
+			bool is_restart_game_button_pressed = false;
+
+			int x_position_of_mouse = 0,
+				y_position_of_mouse = 0;
+
+			
+
+			if(event_of_pressed_button.type == sf::Event::MouseButtonPressed){
+
+				is_mouse_button_pressed = true;
+
+				x_position_of_mouse = event_of_pressed_button.mouseButton.x;
+				y_position_of_mouse = event_of_pressed_button.mouseButton.y;
+				
+			}
+
+			if(event_of_released_button.type == sf::Event::MouseButtonReleased){
+				is_mouse_button_released = true;
+			}
+		
+			if(is_mouse_position_in_district_of_button({x_position_of_mouse,y_position_of_mouse},{x_position_of_button,y_position_of_button},button_height,button_widht) 
+			    && is_mouse_button_pressed)
+			   {
+				   is_restart_game_button_pressed = true;
+			   }
+
+
+			draw_main_menu_picture(window,is_restart_game_button_pressed);
+
+			return is_restart_game_button_pressed && is_mouse_button_released;
+
+		}
+		
+	private:
+
+		double x_position_of_button = 100,y_position_of_button = 550 ,
+				button_height = 50 ,button_widht = 200;
+
+		sf::Texture for_pressed_button,for_not_pressed_button;
+		sf::Sprite pressed_button , not_pressed_button;
+};
+
+void main_cycle(sf::RenderWindow& window, List_of_platforms& platforms,Person& person)
+{
+		float height = 0;
+		sf::Clock clock;
+
+
+		sf::Texture background_texture;
+		background_texture.loadFromFile("images/background.png");
+		sf::Sprite background;
+		background.setTexture(background_texture);
+
+
+		while (window.isOpen())
+		{
+			float dt = clock.getElapsedTime().asSeconds();
+			clock.restart();
+
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed){
+					window.close();
+				}	   
+			}
+
+			if(is_losing_game(person.get_y_position())){
+				break;
+			}
+			update_all_objects(platforms,person,height,dt);
+
+
+			draw_all_objects(window,platforms,person,background);
+		}
+}	
+
+int main(){
+
+
+	Main_menu menu;
+	
+
+	Person person;
+	List_of_platforms platforms;
+
+	
+	sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Moodle Jump");
+	srand(time(nullptr));
+
+	
+	
+	while (window.isOpen()){
+		
+		
+		sf::Event event_of_released_button;
+		sf::Event event_of_pressed_button;
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed){  
+
+				window.close();
+				
+			}
+			if(event.type == sf::Event::MouseButtonPressed){
+
+				event_of_pressed_button = event;
+				
+			}
+			if(event.type == sf::Event::MouseButtonReleased){
+
+				event_of_released_button = event;
+
+			}	
+		}
+
+		
+		if(menu.is_restart_game_button_pressed_and_released(window,event_of_pressed_button,event_of_released_button)){
+			main_cycle(window,platforms,person);
+		}
+	
+	
+	}
 
 	return 0;
+}
 }
